623f9fde8379f49e6e6757b8e336cc9ab4fc7d25
Samuel Jesuthas <samueljesuthas27@gmail.com>
2023-03-15 18:21:10 +0000

    listing subdirectories

--- AssignmentScripts/Directory.sh
+++ AssignmentScripts/Directory.sh
@@ -1,3 +1,37 @@
 #! /usr/bin/env bash
 
+echo ==================================
+echo Listing Directories in _Directory
+echo ==================================
+echo 
+
 du -shc /workspace/utility_script-samjesus8/_Directory/*
+echo
+
+echo =================================
+echo -1st Level of SubDirectories
+echo ==================================
+
+ls /workspace/utility_script-samjesus8/_Directory/*
+echo
+
+echo =================================
+echo -2nd Level of SubDirectories
+echo ==================================
+echo
+
+echo Subdir1
+ls /workspace/utility_script-samjesus8/_Directory/dir1/subdir1/*
+echo
+
+echo Subdir2
+ls /workspace/utility_script-samjesus8/_Directory/dir1/subdir2/*
+echo
+
+echo Subdir3
+ls /workspace/utility_script-samjesus8/_Directory/dir1/subdir3/*
+echo
+
+echo Subdir4
+ls /workspace/utility_script-samjesus8/_Directory/dir1/subdir4/*
+echo
---
+++ AssignmentScripts/forlooptest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for f in $( ls /workspace/utility_script-samjesus8/_Directory ); do
+	echo $f
+done 
