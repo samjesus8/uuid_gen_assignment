60b76b17634b2b7431a22a42e5d9243969da07eb
Samuel Jesuthas <samueljesuthas27@gmail.com>
2023-03-20 14:20:26 +0000

    dir1 listing complete

--- AssignmentScripts/Directory.sh
+++ AssignmentScripts/Directory.sh
@@ -1,64 +1,74 @@
 #! /usr/bin/env bash
 
-directory="/workspace/utility_script-samjesus8/_Directory"
-
 echo ==================================
 echo Listing Directories in _Directory
 echo ==================================
 echo 
 
-du -shc /workspace/utility_script-samjesus8/_Directory/*
+# Set directory paths
+main_dir="/workspace/utility_script-samjesus8/_Directory"
+dir1="/workspace/utility_script-samjesus8/_Directory/dir1"
+dir2="/workspace/utility_script-samjesus8/_Directory/dir2"
+dir3="/workspace/utility_script-samjesus8/_Directory/dir3"
+dir4="/workspace/utility_script-samjesus8/_Directory/dir4"
+
+# List the main _Directory folder
+for directory in $"/workspace/utility_script-samjesus8/_Directory/*"; do
+        du -shc $directory
+    done
 echo
 
-read -p "Please type in the Directory to view: " dirselect
-if [ $dirselect == "dir1" ]; then
+# Input on which subdirectory we want to view
+read -rp "Please type in the Directory to view: " dirselect
 
-    echo ==================================
-    echo Subdirectories of Dir1
-    echo ==================================
+# Outcome Area
+# ============================================================================
+if [ "$dirselect" == "dir1" ]; then
+    echo "=================================="
+    echo "Subdirectories of Dir1"
+    echo "=================================="
+    echo
+
+    # List main subdirectory
+    echo "Size |  Folder Name"
+    for directory in $dir1/*; do
+            du -shc $directory
+        done
+    echo
+
+    # List subsubdirectories
+    echo "=================================="
+    echo "SubSubDirectories of Dir1"
+    echo "=================================="
     echo
     echo "Size |  Folder Name"
-    du -shc $directory/dir1/*
+        for directory in $dir1/*/*; do
+            du -shc $directory
+        done
+    echo
     echo
 
-    # Listing SubSubDirectories of Dir1
-    echo ==================================
-    echo SubSubDirectories of Dir1
-    echo ==================================
-    echo
-    echo "Size |  Folder Name"
-    du -shc $directory/dir1/*/*
-    echo
+    # List files
+    echo "=================================="
+    echo "File Report on SubSubDirs"
+    echo "=================================="
 
-    # Listing Files (bmp/ico/png/dat/jpg)
-    echo ==================================
-    echo File Report on SubSubDirs
-    echo ==================================
-    echo
+    # Count each file by filetype and list this output
+    subdirs=("subsubdir1" "subsubdir2" "subsubdir3" "subsubdir4")
 
-    #du -shc $directory/dir1/subdir1/subsubdir1/*
-    find $directory/dir1/subdir1/subsubdir1/ -maxdepth 1 -type f -printf '%f\n'
+    for subdir in "${subdirs[@]}"; do
+        echo "$subdir"
+        echo "=================================="
+        find "$dir1/subdir1/$subdir/" -type f -printf '%f\n' | awk -F. '{print $NF}' | sort | uniq -c
+        echo "=================================="
+        echo
+    done
 
+# ============================================================================
 
-
-elif [ $dirselect == "dir2" ]; then
-    echo ==================================
-    echo Subdirectories of Dir2
-    echo ==================================
-    echo
-    ls $directory/dir2/*
-elif [ $dirselect == "dir3" ]; then
-    echo ==================================
-    echo Subdirectories of Dir3
-    echo ==================================
-    echo
-    ls $directory/dir3/*
-elif [ $dirselect == "dir4" ]; then
-    echo ==================================
-    echo Subdirectories of Dir4
-    echo ==================================
-    echo
-    ls $directory/dir4/*
 else 
 	echo "Directory does not exist"
+    echo "The directories in _Directory are:"
+    echo
+    ls $main_dir
 fi
